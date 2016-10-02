af6fb8e6b57569825f3c98a48f708bf73d46df4c
Héctor Feliciano Campos Muñoz <BlisS@MBP-de-Hector.lan>
2016-10-02 09:40:20 -0500

    landing 80%

---
+++ app/components/destaca-dos.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+	didInsertElement(){
+		this._super();
+		Ember.run.scheduleOnce('afterRender',this,function(){
+			//jquery
+			 Ember.$("#owl-demo").owlCarousel({
+		 
+		      autoPlay: 3000, //Set AutoPlay to 3 seconds
+		 
+		      navigation : false, // Show next and prev buttons
+		      slideSpeed : 300,
+		      paginationSpeed : 400,
+		      singleItem:false,
+		      items:3
+		  });
+		});
+	}
+});
